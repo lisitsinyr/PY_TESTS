517d18295a15af2cf4df90a6d642d639d162b221
lisitsinyr <lisitsinyr@gmail.com>
2024-04-09 16:58:53 +0400

    20240409165852

--- ListDir/ListDir3.py
+++ ListDir/ListDir3.py
@@ -39,7 +39,7 @@ from LUDoc import *
 #import LUDoc
 import LUErrors
 import LUFile
-#import LUFileUtils
+import LUFileUtils
 import LULog
 import LUNetwork
 import LUNumUtils
@@ -65,144 +65,35 @@ import LUYouTube
 #------------------------------------------
 #CONST
 #------------------------------------------
-GDir: str = ''
-GMask: str = '*.*'
-GDirCount: int = 0
-GLevel: int = 0
 
-# -------------------------------------------------------------------------------
-# WorkFile (AFile_path)
-# -------------------------------------------------------------------------------
-def WorkFile (AFullFileName):
-    # global Shablon
+#------------------------------------------
+# FuncDir ()
+#------------------------------------------
+def FuncDir (ADir: os.DirEntry):
+    """FuncDir"""
 #beginfunction
-    LFileNameSource: str = AFullFileName
-    LFullFileName: str = LFileNameSource
-    LFileName: str = os.path.basename(LFullFileName)
-    LFileSize: int = os.path.getsize(LFullFileName)
-    LFileDir: str = os.path.dirname(LFullFileName)
-
-    #-------------------------------------------------------------------------
-    #LFileTimeSource = GetFileTime(LFileNameSource)
-    #-------------------------------------------------------------------------
-    #file modification
-    LFileTimeSource = os.path.getmtime(LFileNameSource)
-    #convert timestamp into DateTime object
-    LFileTimeSource = datetime.datetime.fromtimestamp(LFileTimeSource)
-    #file creation
-    LFileTimeSource = os.path.getctime(LFileNameSource)
-    #convert creation timestamp into DateTime object
-    LFileTimeSource = datetime.datetime.fromtimestamp(LFileTimeSource)
-
-    # if Shablon == Shablon1:
-    #     #Shablon1: str = '{FullFileDir} {FileName} {FileTime} {FileSize}'
-    #     message = Shablon.format(FullFileDir=LFullFileName,FileName=LFileName,FileTime=LFileTimeSource,FileSize=LFileSize)
-    #     print (message)
-    # #endif
-    # if Shablon == Shablon2:
-    #     #Shablon2: str = '{FileName={FullFileName}|{FullFileDir}|{FileDir}'
-    #     message = Shablon.format(FileName=LFileName,FullFileName=LFullFileName,FullFileDir=LFullFileName,FileDir=LFileDir)
-    #     print (message)
-    # #endif
+    # print ('DEBUG: function ',sys._getframe (0).f_code.co_name, '...')
+    # LULog.LoggerAPPS_AddLevel (LULog.TEXT, ADir.path)
+    # Lstat = os.stat(AFile.name)
+    # print('stat_name:',Lstat)
+    # Lstat = os.stat(AFile.path)
+    # print('stat_path:',Lstat)
+    ...
 #endfunction
 
-#-------------------------------------------------------------------------------
-# ListFile (ASourcePath, AMask)
-#-------------------------------------------------------------------------------
-def ListFile (ASourcePath, AMask, _OutFile, _Option, _FuncDir, _FuncFile):
+#------------------------------------------
+# FuncFile ()
+#------------------------------------------
+def FuncFile (AFile: os.DirEntry):
+    """FuncFile"""
 #beginfunction
-    """
-    if ($OutFile) and (UCase($OutFile) <> "CONSOLE")
-        $HandleFile = FreeFileHandle
-        $Res = Open ($HandleFile, $OutFile, 1+4)
-    #endif
-    """
-
-    #LDay = EncodeDate(@Year,@MonthNo,@MDayNo)
-
-    # LFileMask = os.path.join (ASourcePath, '*.*')
-    LSourcePath = ASourcePath
-
-    LFiles = os.listdir (LSourcePath)
-    LListFile = 0
-    for LFileName in LFiles:
-        LListFile = LListFile + 1
-
-        LFullFileName: str = os.path.join (ASourcePath, LFileName)
-        if os.path.isfile (LFullFileName):
-            #Это файл
-            #Lstats = os.stat (LFullFileName)
-            # LULog.LoggerAPPS_AddLevel (LULog.TEXT, LFullFileName)
-            LULog.LoggerAPPS_AddLevel (LULog.TEXT, LFileName)
-
-            WorkFile (LFullFileName)
-        #endif
-    #endfor
-#endfunction
-
-#-------------------------------------------------------------------------------
-# ListDir (ASourcePath, AMask)
-#-------------------------------------------------------------------------------
-def ListDir (ASourcePath, AMask, _OutFile, _Option, _FuncDir, _FuncFile):
-    global GLevel
-#beginfunction
-    global GLevel
-    global GDirCount
-    GLevel = GLevel + 1
-    #------------------------------------------------------------
-    # Dir
-    #------------------------------------------------------------
-    LFullDirName = ASourcePath
-    # LULog.LoggerAPPS_AddDebug (LFullDirName)
-    LDirName = os.path.basename (ASourcePath)
-    LULog.LoggerAPPS_AddDebug (LDirName)
-    GDirCount = GDirCount + 1
-
-    """
-       # FileCount = ListFile(ASourcePath, AMask, $OutFile, $Option, FuncFile)
-       if $Option = 10 or $Option = 11 or $Option = 12
-          if ($OutFile) and (UCase($OutFile) <> "CONSOLE")
-             $HandleDir = FreeFileHandle
-             $Res = Open ($HandleDir, $OutFile, 1+4)
-          #endif
-          if $OutFile
-             if UCase($OutFile) = "CONSOLE"
-                ? ASourcePath
-             else
-                $Res = WriteLine ($HandleDir, ASourcePath+" "+$DirCount+@CRLF)
-             #endif
-          #endif
-          if ($OutFile) and (UCase($OutFile) <> "CONSOLE")
-             $Res = Close ($HandleDir)
-          #endif
-       #endif
-    """
-
-    LFiles = os.listdir (ASourcePath)
-    for LFile in LFiles:
-        # LSourcePath: str = os.sep.join ([ASourcePath, LFile])
-        LSourcePath = os.path.join (ASourcePath, LFile)
-        #Lstats = os.stat (LSourcePath)
-        if os.path.isdir (LSourcePath):
-            #Это каталог
-            LSourcePath = os.path.join (ASourcePath, LFile)
-            # LULog.LoggerAPPS_AddDebug (LSourcePath)
-            # LULog.LoggerAPPS_AddLevel (LULog.TEXT, LSourcePath)
-
-            ListFile (LSourcePath, AMask, _OutFile, _Option, _FuncDir, _FuncFile)
-            # FileCount = ListFile(LSourcePath, AMask, _OutFile, _Option, _FuncFile)
-
-            #if FuncDir
-            #   $s = '$$Res = FuncDir ($$DirCount, $LSourcePath, $FileCount)'
-            #   $ResExe = execute ($s)
-            ##endif
-
-            #WorkFile(LSourcePath)
-
-            ListDir (LSourcePath, AMask, _OutFile, _Option, _FuncDir, _FuncFile)
-        #endif
-    #endfor
-    GLevel = GLevel - 1
+    # print ('DEBUG: function ',sys._getframe (0).f_code.co_name, '...')
+    # LULog.LoggerAPPS_AddLevel (LULog.TEXT, AFile.path)
+    # Lstat = os.stat(AFile.name)
+    # print('stat_name:',Lstat)
+    # Lstat = os.stat(AFile.path)
+    # print('stat_path:',Lstat)
+    ...
 #endfunction
 
 #------------------------------------------
@@ -214,8 +105,9 @@ def TEST_01 ():
     print ('DEBUG: function ',sys._getframe (0).f_code.co_name, '...')
     PrintInfoObject('-----TEST_01----')
     PrintInfoObject(TEST_01)
-
-    ListDir (GDir, GMask, '', '', '', '')
+    _OutFile = 'ListDir.txt'
+    LUFile.FileDelete (_OutFile)
+    LUFileUtils.ListDir(GDir, GMask, _OutFile, 11, FuncDir, FuncFile)
 #endfunction
 
 #------------------------------------------
@@ -228,19 +120,18 @@ def main ():
 
     LULog.STARTLogging (LULog.TTypeSETUPLOG.tslINI,'LOG_INIT',
                         'LOGGING_FILEINI.log','LOGGING_FILEINI_json.log')
-    # LULog.LoggerTOOLS_AddLevel (LULog.TEXT, "Тест")
-    # LULog.LoggerTOOLS_AddDebug ("debug")
-    # LULog.LoggerTOOLS_AddLevel (logging.DEBUG, "debug")
 
     s = f'sys.argv = {sys.argv}'
     LULog.LoggerAPPS_AddLevel (LULog.TEXT, s)
 
+    #----------------------------------------------------------------
     # LPDir = LUParserARG.GetParam ('PDir', "")
     # s = f'Format = {LPDir}'
     # LULog.LoggerAPPS_AddLevel (LULog.TEXT, LPDir)
     # LPMask = LUParserARG.GetParam ('PMask', "")
     # s = f'PMask = {LPMask}'
     # LULog.LoggerAPPS_AddLevel (LULog.TEXT, LPMask)
+    #----------------------------------------------------------------
 
     Lparser = argparse.ArgumentParser (description = 'Параметры', prefix_chars = '-/')
     Lparser.add_argument ('PDir', type = str, default='', help = 'PDir')
@@ -249,12 +140,14 @@ def main ():
     # Lparser.add_argument ('-PMask', type = str, nargs = '?', default = '', dest = 'PMask', help = 'PMask')
     Largs = Lparser.parse_args ()
     
-    GDir = Largs.PDir
-    GMask = Largs.PMask
     GDir = 'D:\\PROJECTS_LYR\\CHECK_LIST\\05_DESKTOP\\02_Python\\PROJECTS_PY\\TESTS_PY'
+    GDir = 'D:\\GAMES\\WORK\\AV'
+    GDir = Largs.PDir
+    LULog.LoggerAPPS_AddLevel (LULog.TEXT, f'PDir = {GDir}')
+
     GMask = '*.*'
-    LULog.LoggerAPPS_AddLevel (LULog.TEXT, f'GDir = {GDir}')
-    LULog.LoggerAPPS_AddLevel (LULog.TEXT, f'GMask = {GMask}')
+    GMask = Largs.PMask
+    LULog.LoggerAPPS_AddLevel (LULog.TEXT, f'PMask = {GMask}')
 
     TEST_01 ()
 
