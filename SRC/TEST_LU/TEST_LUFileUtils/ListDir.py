64f7b5f7446ae71fab6997587fd7a013a91c104a
lisitsinyr <lisitsinyr@gmail.com>
2024-04-22 16:24:33 +0400

    20240422162432

--- SRC/TEST_LU/TEST_LUFileUtils/ListDir.py
+++ SRC/TEST_LU/TEST_LUFileUtils/ListDir.py
@@ -91,7 +91,8 @@ def main():
 #beginfunction
     # print ('DEBUG: function ',sys._getframe (0).f_code.co_name, '...')
 
-    LULog.STARTLogging (LULog.TTypeSETUPLOG.tslINI,'LOG_INIT',
+    LULog.STARTLogging (LULog.TTypeSETUPLOG.tslINI,
+                        r'D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\02_Python\PROJECTS_PY\TESTS_PY\LOG',
                         'LOGGING_FILEINI.log','LOGGING_FILEINI_json.log')
 
     LUDoc.PrintInfoObject('-----main----')
